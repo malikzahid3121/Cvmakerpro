506d7b8fc6a561df860fa1a8b243aae8e1eb8757
malikzahid3121 <malikzahid7754172@gmail.com>
2026-07-12 23:47:08 +0400

    Create PreviewActivity.java

---
+++ app/src/main/java/com/cvmakerpro/app/PreviewActivity.java
@@ -0,0 +1,42 @@
+package com.cvmakerpro.app;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PreviewActivity extends AppCompatActivity {
+
+    TextView tvPreview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview);
+
+        tvPreview = findViewById(R.id.tvPreview);
+
+        String name = getIntent().getStringExtra("name");
+        String email = getIntent().getStringExtra("email");
+        String phone = getIntent().getStringExtra("phone");
+        String education = getIntent().getStringExtra("education");
+        String experience = getIntent().getStringExtra("experience");
+        String skills = getIntent().getStringExtra("skills");
+
+
+        String cv = 
+                "CURRICULUM VITAE\n\n" +
+                "Name: " + name + "\n" +
+                "Email: " + email + "\n" +
+                "Phone: " + phone + "\n\n" +
+                "EDUCATION\n" +
+                education + "\n\n" +
+                "WORK EXPERIENCE\n" +
+                experience + "\n\n" +
+                "SKILLS\n" +
+                skills;
+
+
+        tvPreview.setText(cv);
+    }
+}

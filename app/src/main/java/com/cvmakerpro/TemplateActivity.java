91d4df580f08fb8dcd897e72d2fb2037aba80a76
malikzahid3121 <malikzahid7754172@gmail.com>
2026-07-14 00:34:46 +0400

    Update TemplateActivity.java

--- app/src/main/java/com/cvmakerpro/TemplateActivity.java
+++ app/src/main/java/com/cvmakerpro/TemplateActivity.java
@@ -1,5 +1,6 @@
 package com.cvmakerpro.app;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
@@ -10,34 +11,69 @@ public class TemplateActivity extends AppCompatActivity {
 
     Button btnModern, btnClassic, btnSimple;
 
+    SharedPreferences preferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_template);
+
 
         btnModern = findViewById(R.id.btnModern);
         btnClassic = findViewById(R.id.btnClassic);
         btnSimple = findViewById(R.id.btnSimple);
 
 
+        preferences = getSharedPreferences(
+                "CVMakerPro",
+                MODE_PRIVATE
+        );
+
+
         btnModern.setOnClickListener(v -> {
+
+            saveTemplate("Modern");
+
             Toast.makeText(this,
                     "Modern Template Selected",
                     Toast.LENGTH_SHORT).show();
+
+            finish();
         });
 
 
         btnClassic.setOnClickListener(v -> {
+
+            saveTemplate("Classic");
+
             Toast.makeText(this,
                     "Classic Template Selected",
                     Toast.LENGTH_SHORT).show();
+
+            finish();
         });
 
 
         btnSimple.setOnClickListener(v -> {
+
+            saveTemplate("Simple");
+
             Toast.makeText(this,
                     "Simple Template Selected",
                     Toast.LENGTH_SHORT).show();
+
+            finish();
         });
+
     }
+
+
+    private void saveTemplate(String template){
+
+        preferences.edit()
+                .putString("template", template)
+                .apply();
+
+    }
+
 }
